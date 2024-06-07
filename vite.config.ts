af658b492fb8dfa7a091af6dad0dcf7bd49747b0
thirusakthi-dev <thirusakthi.dev@gmail.com>
2024-06-07 14:51:32 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Thiru-Portfolio",
+  base: "/Thiru-Dev-Portfolio",
 });
